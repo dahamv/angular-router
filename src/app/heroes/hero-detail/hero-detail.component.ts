2872a24c1aa30b1f33205d9da82f7f59fd894429
dahamv <dahamviharana@gmail.com>
2020-10-16 12:00:40 +0000

    experiments

--- src/app/heroes/hero-detail/hero-detail.component.ts
+++ src/app/heroes/hero-detail/hero-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { HeroService } from '../hero.service';
+import { Observable } from 'rxjs';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-hero-detail',
@@ -6,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroDetailComponent implements OnInit {
 
-  constructor() { }
+  hero$ : Observable<Hero>
+  heroId : number = null;
+  heroName : string = null;
+
+  constructor(  private route: ActivatedRoute,
+                private router: Router,
+                private service: HeroService) {}
 
   ngOnInit(): void {
+    this.hero$ = this.route.paramMap.pipe(
+    switchMap((params: ParamMap) =>
+      this.service.getHero(+params.get('id')))
+    );
+    this.hero$.subscribe(hero => {this.heroId = hero.id; this.heroName = hero.name ; console.log( hero.id + hero.name) })
   }
 
+  gotoHeroes() {
+    this.router.navigate(['/heroes']);
+  }
 }
