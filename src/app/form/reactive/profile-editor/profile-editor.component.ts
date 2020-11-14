429c8a245d3501124fd0f29f4b6ec6a6faa2a9fc
dahamv <dahamviharana@gmail.com>
2020-11-14 05:29:03 +0000

    next step done

--- src/app/form/reactive/profile-editor/profile-editor.component.ts
+++ src/app/form/reactive/profile-editor/profile-editor.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
   templateUrl: './profile-editor.component.html',
   styleUrls: ['../../form.component.scss']
 })
-export class ProfileEditorComponent implements OnInit {
+export class ProfileEditorComponent {
 
-  constructor() { }
+  profileForm = new FormGroup({
+                        firstName: new FormControl(''),
+                        lastName: new FormControl(''),
+                    });
 
-  ngOnInit(): void {
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    alert(this.profileForm.value);
   }
 
 }
